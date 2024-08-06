d0a522ae37c739697586c1b8aa0669623602d275
Sadia-H <sadiahussain118@gmail.com>
2024-08-06 15:34:31 +0100

    Updated test suite

--- src/test/java/com/sakilaTests/SetUpTests.java
+++ src/test/java/com/sakilaTests/SetUpTests.java
@@ -2,23 +2,22 @@ package com.sakilaTests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 public class SetUpTests {
     protected WebDriver driver;
 
-    @BeforeSuite
+    @BeforeClass
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
     }
 
-//    @AfterSuite
-//    public void tearDown() {
-//        System.out.println("*** tear down ***");
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
+    @AfterClass
+    public void tearDown() {
+        System.out.println("*** tear down ***");
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
