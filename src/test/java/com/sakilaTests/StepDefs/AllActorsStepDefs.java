fbef6ae04c8180bc90e792078a2242ae61759fcf
Sadia-H <sadiahussain118@gmail.com>
2024-08-07 15:51:09 +0100

    Fixed all actors scenario

--- src/test/java/com/sakilaTests/StepDefs/AllActorsStepDefs.java
+++ src/test/java/com/sakilaTests/StepDefs/AllActorsStepDefs.java
@@ -1,25 +1,27 @@
-//package com.sakilaTests.StepDefs;
-//
-//import io.cucumber.java.After;
-//import io.cucumber.java.Before;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
-//
-//import java.time.Duration;
-//import java.util.List;
-//
-//public class AllActorsStepDefs {
-//
+package com.sakilaTests.StepDefs;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.List;
+import static com.sakilaTests.StepDefs.CommonStepDefs.driver;
+import static com.sakilaTests.StepDefs.CommonStepDefs.wait;
+
+public class AllActorsStepDefs {
+
 //    private WebDriver driver;
 //    private WebDriverWait wait;
-//
+
 //    @Before
 //    public void setUp() {
 //        System.setProperty("webdriver.chrome.driver", "src/test/java/resources/TestSuites/chromeDriver/chromedriver.exe");
@@ -35,17 +37,17 @@
 //            driver.quit();
 //        }
 //    }
-//
-////    @Given("the page {string} is loaded")
-////    public void thePageIsLoaded(String url) {
-////        driver.get(url);
-////        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("navbarContainer")));
-////    }
-//
-//    @Then("all actors should be listed on the page")
-//    public void allActorsShouldBeListedOnThePage() {
-//        List<WebElement> actorElements = driver.findElements(By.className("filmActorBlockLink"));
-//        Assert.assertTrue(actorElements.size() > 0, "There should be at least one actor displayed.");
-//
+
+//    @Given("the page {string} is loaded")
+//    public void thePageIsLoaded(String url) {
+//        driver.get(url);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("navbarContainer")));
 //    }
-//}
+
+    @Then("all actors should be listed on the page")
+    public void allActorsShouldBeListedOnThePage() {
+        List<WebElement> actorElements = driver.findElements(By.className("filmActorBlockLink"));
+        Assert.assertTrue(actorElements.size() > 0, "There should be at least one actor displayed.");
+
+    }
+}
--- src/test/java/resources/TestSuites/features/AllActors.feature
+++ src/test/java/resources/TestSuites/features/AllActors.feature
@@ -1,5 +1,5 @@
-#Feature: All Actors
-#
-#  Scenario: Display and navigate through all actors page
-#    Given the page "http://localhost:5173/all-actors" is loaded
-#    Then all actors should be listed on the page
+Feature: All Actors
+
+  Scenario: Display and navigate through all actors page
+    Given the page "http://localhost:5173/all-actors" is loaded
+    Then all actors should be listed on the page
